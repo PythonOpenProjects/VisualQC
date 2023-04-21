b3fdd8d70ef72fb8ef88ad3e85a6f5e4bccefbf4
PythonOpenProjects <128476143+PythonOpenProjects@users.noreply.github.com>
2023-04-21 10:20:22 +0200

    Update VisualQC.py
    Cleaned code and bugs fixed

--- VisualQC.py
+++ VisualQC.py
@@ -18,13 +18,10 @@ from tkinter.filedialog import askopenfilename
 #from matplotlib.widgets import RectangleSelector
 import matplotlib.pyplot as plt
 from matplotlib import style
-#from matplotlib.widgets import SpanSelector
-#import matplotlib.image as image
-#from matplotlib.widgets import Button as btn
+
 from ttkthemes import ThemedTk
 from tkinter import messagebox
-#import warnings
-#warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 
 
@@ -35,7 +32,7 @@ checked=0
 rowCoord=0
 MyQC="0"
 flagNewCol=0
-#MyQCFromBTN="0"
+
 def esegui():
     
     action=hiddenLabel["text"]
@@ -47,41 +44,29 @@ def esegui():
                                 title = "Choose a file."
                                 )
     
-    #df = pd.read_csv(name)
+    
     sheet = Sheet(LabelFrameXls,
                                data = pd.read_excel(name,      # filepath here
                                                     #sheet_name = "sheet1", # optional sheet name here
                                                     engine = "openpyxl",
                                                     header = None).values.tolist())
-    #sheet.change_theme("dark")
+    
     sheet.enable_bindings("all")
     
-    #print("flagnewcol"+str(flagNewCol))
-    #print("MIN Value = " + str(v1.get()))
-    #print("MAX Value = " + str(v2.get()))
-    #print("Spike Value = " + str(v1Spike.get()))
+
     
     def rc():
         try:
             
-            #df = pd.DataFrame(sheet.get_column_data(0))
             XLSoutputName = str(time.strftime("%Y%m%d%H%M%S")+'.xlsx')
-            #CSVoutputName = str(time.strftime("%Y%m%d%H%M%S")+'.csv')
             df = pd.DataFrame(sheet.get_sheet_data(return_copy = True, get_header = True, get_index = False))
-            #df.to_cvs(CSVoutputName, encoding='utf-8')
             df.to_excel(XLSoutputName,sheet_name='Sheet_name_1')
             
         except:
             print("An exception occurred")
-        #all_data = sheet.get_sheet_data(return_copy = True, get_header = True, get_index = False)
-        #df = pd.DataFrame(all_data)
-        #print(df)
-        
-        #numCols=sheet.get_total_cols()
-        #print(str(numCols))
+
             
     def newcol():
-        #sheet.insert_column(values = None, idx = 1, width = None, deselect_all = False, add_rows = True, equalize_data_row_lengths = True,mod_column_positions = True, redraw = False)
         tmpMyCol=int(LETTERS_ARRAY.index(enQCCol.get()))-1
         tmpMyColQC=int(LETTERS_ARRAY.index(enQCColCheck.get()))-1
         print("Selected column QC in"+str(tmpMyCol))
@@ -103,12 +88,7 @@ def esegui():
         if tmpMyColQC==-1:
             #print("Warning: QC column is not defined")
             messagebox.showwarning("showwarning", "Warning: QC column is not defined")
-        if v1.get()==0 and varOkQC4.get()==1:
-            messagebox.showwarning("showwarning", "Warning: MIN value is zero")
-        if v2.get()==0 and varOkQC4.get()==1:
-            messagebox.showwarning("showwarning", "Warning: MAX value is zero")
-        if v1Spike.get()==0 and varOkQC3.get()==1:
-            messagebox.showwarning("showwarning", "Warning: SPIKE value is zero")
+
             
         try:
             countRow=0
@@ -152,7 +132,7 @@ def esegui():
                             
                             if spike >float(l1Spike.get()) and varOkQC3.get()==1:
                                 QcValue=3
-                                sheet.set_cell_data(countRow-1,tmpMyColQC, QcValue)
+                                sheet.set_cell_data(countRow,tmpMyColQC, QcValue)
                                 sheet.highlight_cells(row = countRow, column = tmpMyColQC, cells = [], canvas = "table", bg = "red", fg = None, redraw = False, overwrite = True)
                         
                             else:
@@ -169,14 +149,13 @@ def esegui():
                             sheet.set_cell_data(countRow,tmpMyColQC, 0)
                 countRow+=1
         except Exception as e:
-            #print("An exception occurred Beta")
+            #print("An exception occurred ")
             #print(e)
             messagebox.showwarning("showwarning", "Warning: "+e)
             
             
     sheet.popup_menu_add_command("Save all as XLSX", rc, table_menu = True, index_menu = True, header_menu = True)
     sheet.popup_menu_add_command("Use Automatic QC assignement", newcol, table_menu = True, index_menu = True, header_menu = True)
-    #frame.grid(row = 0, column = 0, sticky = "nswe")
     sheet.grid(row = 0, column = 0, sticky = "nswe")
     sheet.refresh(redraw_header = True, redraw_row_index = True)
     
@@ -194,9 +173,6 @@ def esegui():
             colNum=i
             print(colNum)
             
-        
-        
-        #print (sheet.get_column_data(colNum))
         noLabel=0
         tmpLabel=''
         n=[]
@@ -214,18 +190,12 @@ def esegui():
                 
             checked=0
         except Exception as e:
-            #print("An exception occurred Beta")
+            #print("An exception occurred")
             #print(e)
             messagebox.showwarning("showwarning", "Warning: "+e)
             checked=1
-        # If there is a selected cell, get the data for it
-        '''
-        if sheet.cell_selected(row, column):
-            value = sheet.get_cell_data(row, column, return_copy = True)
-            print(value)
-        else:
-            print("No cell is selected")
-        '''
+        
+        
         def on_pick(event):
             
             checkQCColor=MyQC.get()
@@ -234,17 +204,10 @@ def esegui():
             xmouse, ymouse = event.mouseevent.xdata, event.mouseevent.ydata
             x, y = artist.get_xdata(), artist.get_ydata()
             ind = event.ind
-            #print('Artist picked:', event.artist)
-            #print('{} vertices picked'.format(len(ind)))
-            #print('Pick between vertices {} and {}'.format(min(ind), max(ind)+1))
-            #print('x, y of mouse: {:.2f},{:.2f}'.format(xmouse, ymouse))
-            #print('Data point:', x[ind[0]], y[ind[0]])
+
             ax.plot(x[ind[0]], y[ind[0]], 'b*')
             ax.annotate(f"QC "+checkQCColor, (x[ind[0]], y[ind[0]]),color='Black')
             event.canvas.draw()
-            #rowCoord=int(x[ind[0]]+1)
-            #global MyQCFromBTN
-            #print(MyQCFromBTN)
             
                 
                 
@@ -274,17 +237,9 @@ def esegui():
                 sheet.highlight_cells(row = int(x[ind[0]]+1), column = colNum+1, cells = [], canvas = "table", bg = "white", fg = None, redraw = False, overwrite = True)
             if checkQCColor=="Q":
                 sheet.highlight_cells(row = int(x[ind[0]]+1), column = colNum+1, cells = [], canvas = "table", bg = "blue", fg = None, redraw = False, overwrite = True)
-        '''
-        def _yes(event):
-            if event.button == 3:
-                #print("yolo")
-                global MyQCFromBTN
-                MyQCFromBTN="3"
-                print(MyQCFromBTN)
-        '''
+
                 
         if checked==0:
-            #DarkestorNot=0
             DarkestorNot=varShowDarkest.get()
             if DarkestorNot == 1:
                 MyThemeTMP=MyTheme.get()
@@ -302,8 +257,7 @@ def esegui():
             if QCorNot == 1:
                 for index in range(len(n)):
                     showQCval="QC "+str(sheet.get_cell_data(index+1, colNum+1))
-                    #showQCval=sheet.get_cell_data(index+1, colNum+1)
-                    #ax.text(index, n[index], n[index], size=12)
+
                     qccolor=""
                     if showQCval=="QC 1":
                         qccolor="green"
@@ -332,23 +286,10 @@ def esegui():
                     
                     ax.text(index, n[index], showQCval, size=12,color =qccolor, fontweight ='bold', clip_on=True)
                     
-            
-            
-                
-            #for r in len(n):
-            #for r in sheet.get_column_data(colNum):
-                
-                #ax.annotate(f"QC "+str(sheet.get_cell_data(0, 0)), (x[n], y[n[r]]),color='Black')
-                #plt.text(int(r+1),int(n[r+1]),"QC "+str(sheet.get_cell_data(r+1, colNum+1)))
-                #plt.text(int(r+1),n[r+1],"QC ")
-                #plt.annotate("QC ", (str(int(r+1)),str(n[r+1])))
+
                 
             fig.canvas.callbacks.connect('pick_event', on_pick)
-            '''
-            axcut = plt.axes([0.9, 0.0, 0.1, 0.075])
-            bcut = btn(axcut, 'QC 3', color='white', hovercolor='green')
-            bcut.on_clicked(_yes)
-            '''
+
             GRIDorNot=varShowGrid.get()
             if GRIDorNot == 1:
                 plt.grid()
@@ -368,26 +309,13 @@ def printnvar():
         
         df = pd.DataFrame(sheet.get_column_data(0))
     except Exception as e:
-        #print("An exception occurred Beta")
+        #print("An exception occurred")
         #print(e)
         messagebox.showwarning("showwarning", "Warning: "+e)
-    #all_data = sheet.get_sheet_data(return_copy = True, get_header = True, get_index = False)
-    #df = pd.DataFrame(all_data)
+
     print(df)
 
-#root = tk.Tk()
-#style = ttk.Style(root)
-#For windows themes: ('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative')
-#style.theme_use('xpnative')
 
-
-#https://wiki.tcl-lang.org/page/List+of+ttk+Themes
-#root = ThemedTk(theme="yaru")
-#root = ThemedTk(theme="itft1")
-#root = ThemedTk(theme="adapta")
-#root = ThemedTk(theme="arc")
-#root = ThemedTk(theme="breeze")
-#root = ThemedTk(theme="plastik")
     
 root = ThemedTk(theme="radiance")
 root.title('Visual QC')
@@ -433,26 +361,9 @@ labelINFO = Label(LabelFrameInfo, text=mytext, bg="sienna3", justify="left", fg=
 labelINFO['text'] = 'QC VALUES:\n\n*QC 0 - NO QUALITY CONTROL \n*QC 1 - GOOD VALUE \n*QC 2 - PROBABLY GOOD VALUE \n*QC 3 - PROBABLY BAD VALUE \n*QC 4 - BAD VALUE \n*QC 5 - CHANGED VALUE \n*QC 6 - VALUE BELOW DETECTION \n*QC 7 - VALUE IN EXCESS \n*QC 8 - INTERPOLATED VALUE \n*QC 9 - MISSING VALUE \n*QC A - PHENOMENON UNCERTAIN \n*QC Q - UNDER DETECTION VALUE \n'
 labelINFO.grid(row=3, column=1, columnspan=5, rowspan=10)
 
- 
-
-
-
-
-
-
-
 
 LabelQC = Label(LabelFrameXls, text ="Select the QC value for the visual check")
 LabelQC.grid(row=1, column=0, sticky=W) 
-
-'''
-frame.pack(fill='both', expand=True)
-frame.grid_columnconfigure(0, weight = 1)
-frame.grid_rowconfigure(0, weight = 1)
-'''
-
-
-
 
 
 # Dropdown menu options
@@ -470,7 +381,7 @@ options = [
     "A",
     "Q",
 ]
-# datatype of menu text
+
 MyQC = StringVar()
 # initial menu text
 MyQC.set( "0" )
@@ -517,8 +428,7 @@ hiddenLabel = Label(LabelFrameAutoQC, text ="load")
 hiddenLabel.grid(row=1, column=0, sticky=W) 
 hiddenLabel.grid_forget()
 
-#LabelMainC = Label(LabelFrameInfo, text ="Use this area\n for the automatic check")
-#LabelMainC.grid(row=0, column=2, sticky=W) 
+
 '''
 START HORIZONTAL SCALE VALUE (for ranges)
 '''
@@ -556,7 +466,7 @@ v2 = DoubleVar()
 def show1outMax():  
       
     sel2 = str(v2.get())
-    #l2.config(text = sel2, font =("Courier", 8))
+    
     l2.delete(0,END)
     l2.insert(0,sel2)
 
@@ -583,7 +493,7 @@ v1Spike = DoubleVar()
 def show1spike():  
       
     selSpike = str(v1Spike.get())
-    #l1Spike.config(text = selSpike, font =("Courier", 8))
+    
     l1Spike.delete(0,END)
     l1Spike.insert(0,selSpike)
 
@@ -617,30 +527,12 @@ varOkQC9 = IntVar()
 buttonvarOkQC9 = Checkbutton(LabelFrameAutoQC, text="Check QC 9", variable=varOkQC9)
 buttonvarOkQC9.grid(row=10, column=1,columnspan=10, sticky=W)
 
-'''
-def executeAuto():
-    colQC=enQCCol.get()
-    tmpMyCol=int(LETTERS_ARRAY.index(enQCCol.get()))-1
-    tmpMyColQC=int(LETTERS_ARRAY.index(enQCColCheck.get()))-1
-    print("Selected column"+str(tmpMyCol))
-    print("Selected column"+str(tmpMyColQC))
-    print("MIN Value = " + str(v1.get()))
-    print("MAX Value = " + str(v2.get()))
-    print("Spike Value = " + str(v1Spike.get()))
-    #insert_column(values = None, idx = "end", width = None, deselect_all = False, add_rows = True, equalize_data_row_lengths = True,mod_column_positions = True, redraw = False)
-    #sheet.insert_column(values = None, idx = "end", width = None, deselect_all = False, add_rows = True, equalize_data_row_lengths = True,mod_column_positions = True, redraw = False)
-    #myfoo = types.FunctionType(esegui.__code__.co_consts[1], {})
-    #flagNewCol=1
-    hiddenLabel.config(text = "newcolumn")
-    esegui()
-'''
-    
+
     
 NUMBERS_ARRAY = []
 
 for n in range(150):
     NUMBERS_ARRAY.append(n)
-
 
 
 TMP_LETTERS_ARRAY = [
@@ -703,9 +595,6 @@ enQCColCheck = Spinbox(LabelFrameAutoQC, values=LETTERS_ARRAY, textvariable=temp
 entriesQCColVarsCheck.append(tempentriesQCColVarsCheck)
 enQCColCheck.grid(row=12, column=2,columnspan=10, sticky=W)
 
-#SelectButton = Button(LabelFrameAutoQC, text="Execute auto QC", command=(executeAuto))
-#SelectButton.grid(row=13, column=1, sticky=W)
-
 
 import matplotlib.cm as cm
 import matplotlib.font_manager
@@ -748,24 +637,7 @@ def create_text_axes(fig, height_px):
     ax.autoscale()
 
 def splash_screen(height_px, lw_bars, lw_grid, lw_border, rgrid, with_text=False):
-    """
-    Create a full figure with the Matplotlib logo.
-
-    Parameters
-    ----------
-    height_px : int
-        Height of the figure in pixel.
-    lw_bars : float
-        The linewidth of the bar border.
-    lw_grid : float
-        The linewidth of the grid.
-    lw_border : float
-        The linewidth of icon border.
-    rgrid : sequence of float
-        The radial grid positions.
-    with_text : bool
-        Whether to draw only the icon or to include 'matplotlib' as text.
-    """
+    
     dpi = 100
     height = height_px / dpi
     figsize = (5 * height, height) if with_text else (height, height)
@@ -775,21 +647,12 @@ def splash_screen(height_px, lw_bars, lw_grid, lw_border, rgrid, with_text=False
     if with_text:
         create_text_axes(fig, height_px)
     ax_pos = (0.535, 0.12, .17, 0.75) if with_text else (0.03, 0.03, .94, .94)
-    #ax = create_icon_axes(fig, ax_pos, lw_bars, lw_grid, lw_border, rgrid)
 
     return fig, ax_pos
 splash_screen(height_px=110, lw_bars=0.7, lw_grid=0.5, lw_border=1,
           rgrid=[1, 3, 5, 7], with_text=True)
 plt.show()
-#sheet.show()
-'''
-fileimg="logo.png"
-logo=image.imread(fileimg)
-plt.imshow(logo)
-plt.show()
-'''
+
 
 
 root.mainloop()
-
-
